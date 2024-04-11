7c94bd4f221b3c4e4d3b2af0e33c35577cf8c9e8
Gersi.doko <Gersi.doko@unh.edu>
2024-04-10 20:46:18 -0400

    add init fish conf

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,12 @@
+fish_add_path ~/.cargo/bin
+zoxide init fish | source
+alias j=__zoxide_z
+alias ji=__zoxide_zi
+alias v=nvim
+alias resetFish="source ~/.config/fish/config.fish"
+alias update="sudo pacman -Syu && yay -Syu"
+alias icat="kitten icat"
+alias lg="lazygit"
+export EDITOR=nvim
+export NVIM_APPNAME="kick"
+
