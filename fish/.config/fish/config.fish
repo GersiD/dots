9cfcaefba5e2e1210d520c8b5bf2ee5ccb7ec49e
Gersi.doko <Gersi.doko@unh.edu>
2024-05-02 21:45:00 -0400

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,6 +7,24 @@ alias resetFish="source ~/.config/fish/config.fish"
 alias update="sudo pacman -Syu && yay -Syu"
 alias icat="kitten icat"
 alias lg="lazygit"
+alias ls="exa --icons"
+alias l="exa --icons"
+if test "$TERM" = "xterm-kitty"
+  alias ssh="kitten ssh"
+  alias mpra="kitten ssh gersi@mp-sys76.cs.unh.edu"
+else
+  alias ssh="ssh"
+  alias mpra="ssh gersi@mp-sys76.cs.unh.edu"
+end
 export EDITOR=nvim
 export NVIM_APPNAME="kick"
+# export NVIM_APPNAME="nvim" # lazyvim
 
+
+# https://starship.rs/
+starship init fish | source
+
+function fish_greeting
+  fastfetch
+end
+set fish_greeting
