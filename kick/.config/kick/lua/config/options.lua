d5a9ae46219f9ed8ce321b20a11b7b1eb857643f
Gersi.doko <Gersi.doko@unh.edu>
2024-07-02 20:28:46 -0400

    wrap lines

--- kick/.config/kick/lua/config/options.lua
+++ kick/.config/kick/lua/config/options.lua
@@ -118,7 +118,12 @@ opt.undolevels = 10000
 opt.updatetime = 200 -- Save swap file and trigger CursorHold
 opt.wildmode = 'longest:full,full' -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
-opt.wrap = false -- Disable line wrap
+opt.textwidth = 120 -- Maximum width of text
+opt.wrap = true -- enable line wrap
+opt.wrapmargin = 2 -- wrap lines at 80 characters
+opt.linebreak = true -- wrap lines at convenient points
+opt.columns = 120
+-- opt.colorcolumn = '100' -- highlight column 80
 
 if vim.fn.has('nvim-0.9.0') == 1 then
   opt.splitkeep = 'screen'
