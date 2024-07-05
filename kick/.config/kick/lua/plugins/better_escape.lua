d536e306e7f8867728938faac244f440b27cba88
Gersi.doko <Gersi.doko@unh.edu>
2024-07-05 15:47:44 -0400

    better escape update kick

--- kick/.config/kick/lua/plugins/better_escape.lua
+++ kick/.config/kick/lua/plugins/better_escape.lua
@@ -1,10 +1,10 @@
 return {
-  "max397574/better-escape.nvim",
-  event = "InsertEnter",
+  'max397574/better-escape.nvim',
+  event = 'InsertEnter',
   config = function()
-    require("better_escape").setup({
+    require('better_escape').setup({
       -- defaults
-      mapping = { "jk", "jj", "kj" },
+      -- mapping = { "jk", "jj", "kj" },
       -- timeout = vim.o.timeoutlen,
       -- clear_empty_lines = true,
       -- keys = "<Esc>",
