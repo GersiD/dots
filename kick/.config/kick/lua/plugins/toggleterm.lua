8de34fa8d710233e263d8c707a14f887f9bb0810
Gersi.doko <Gersi.doko@unh.edu>
2024-07-09 16:41:47 -0400

    toggleterm format

--- kick/.config/kick/lua/plugins/toggleterm.lua
+++ kick/.config/kick/lua/plugins/toggleterm.lua
@@ -1,17 +1,17 @@
 return {
-  "akinsho/toggleterm.nvim",
-  cmd = "ToggleTerm",
+  'akinsho/toggleterm.nvim',
+  cmd = 'ToggleTerm',
   opts = {
-    direction = "horizontal",
+    direction = 'horizontal',
     float_opts = {
-      border = "curved",
-      highlights = { border = "Normal", background = "Normal" },
-      width = 1000,
-      height = 100,
+      border = 'curved',
+      highlights = { border = 'Normal', background = 'Normal' },
+      -- width = 1000,
+      -- height = 100,
     },
     -- size = 10,
     shading_factor = -10,
     -- start_in_insert = true,
-    shell = "pwsh",
+    shell = 'pwsh',
   },
 }
