1795cae899c4449426f5d0ae3833bcdc94e50552
Gersi.doko <Gersi.doko@unh.edu>
2024-07-16 20:26:04 -0400

    move to none-ls

--- kick/.config/kick/lua/plugins/lsp/null_ls.lua
+++ kick/.config/kick/lua/plugins/lsp/null_ls.lua
@@ -1,11 +1,11 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "mason.nvim" },
+  'nvimtools/none-ls.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  dependencies = { 'mason.nvim' },
   opts = function()
-    local nls = require("null-ls")
+    local nls = require('null-ls')
     return {
-      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+      root_dir = require('null-ls.utils').root_pattern('.null-ls-root', '.neoconf.json', 'Makefile', '.git'),
       sources = {
         -- nls.builtins.formatting.fish_indent,
         -- nls.builtins.diagnostics.fish,
