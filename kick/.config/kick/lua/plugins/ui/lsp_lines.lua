05614aec40526230acf7bd3e594abd97cd8c3ec5
Gersi.doko <Gersi.doko@unh.edu>
2024-07-16 20:27:16 -0400

    use inline diagnostics for a bit lets see how it goes

--- kick/.config/kick/lua/plugins/ui/lsp_lines.lua
+++ kick/.config/kick/lua/plugins/ui/lsp_lines.lua
@@ -1,11 +1,21 @@
 return {
-  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-  event = "BufReadPre",
-  config = function()
-    vim.diagnostic.config({
-      underline = true,
-      virtual_text = false,
-    })
-    require("lsp_lines").setup()
-  end,
+  {
+    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+    enabled = false,
+    event = 'BufReadPre',
+    config = function()
+      require('lsp_lines').setup()
+    end,
+  },
+  {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('tiny-inline-diagnostic').setup({
+        options = {
+          multiple_diag_under_cursor = true,
+        },
+      })
+    end,
+  },
 }
