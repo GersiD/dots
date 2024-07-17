b30c2228949267474c87c86c8700fb634907b1c5
Gersi.doko <Gersi.doko@unh.edu>
2024-07-17 16:57:40 -0400

    need lsplines

--- kick/.config/kick/lua/plugins/ui/lsp_lines.lua
+++ kick/.config/kick/lua/plugins/ui/lsp_lines.lua
@@ -1,21 +1,9 @@
 return {
   {
     'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-    enabled = false,
     event = 'BufReadPre',
     config = function()
       require('lsp_lines').setup()
-    end,
-  },
-  {
-    'rachartier/tiny-inline-diagnostic.nvim',
-    event = 'VeryLazy',
-    config = function()
-      require('tiny-inline-diagnostic').setup({
-        options = {
-          multiple_diag_under_cursor = true,
-        },
-      })
     end,
   },
 }
