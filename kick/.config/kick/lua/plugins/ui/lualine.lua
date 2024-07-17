cc7bc5f9fb34c5cb496fc7b3b15d1a4055e049c7
Gersi.doko <Gersi.doko@unh.edu>
2024-07-17 19:49:45 -0400

    new lualine icons

--- kick/.config/kick/lua/plugins/ui/lualine.lua
+++ kick/.config/kick/lua/plugins/ui/lualine.lua
@@ -24,7 +24,7 @@ return {
             },
           },
           { 'filetype', icon_only = true, separator = '', padding = { left = 1, right = 0 } },
-          { 'filename', path = 1, symbols = { modified = '  ', readonly = '', unnamed = '' } },
+          { 'filename', path = 1, symbols = { modified = '  ', readonly = ' ', unnamed = '' } },
           -- stylua: ignore
           {
             function() return require("nvim-navic").get_location() end,
@@ -85,7 +85,7 @@ return {
         },
         lualine_z = {
           function()
-            return ' ' .. os.date('%R')
+            return '󱑍 ' .. os.date('%R')
           end,
         },
       },
