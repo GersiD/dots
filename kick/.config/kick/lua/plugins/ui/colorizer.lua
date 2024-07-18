0cbe0dec9445865f620adaf006d29245c6f9e994
Gersi.doko <Gersi.doko@unh.edu>
2024-07-18 15:11:43 -0400

    colorizer notifications

--- kick/.config/kick/lua/plugins/ui/colorizer.lua
+++ kick/.config/kick/lua/plugins/ui/colorizer.lua
@@ -6,8 +6,10 @@ return {
       function()
         if not require('colorizer').is_buffer_attached(0) then
           require('colorizer').attach_to_buffer(0)
+          vim.notify(' Colorizer enabled', vim.log.levels.INFO, { title = 'Colorizer' })
         else
           require('colorizer').detach_from_buffer(0)
+          vim.notify(' Colorizer disabled', vim.log.levels.INFO, { title = 'Colorizer' })
         end
       end,
       desc = 'UI Colorizer',
