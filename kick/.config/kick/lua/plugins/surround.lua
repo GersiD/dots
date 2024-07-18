b528b59e3a34f36d68f5757919ef4c80c1f5629c
Gersi.doko <Gersi.doko@unh.edu>
2024-07-18 15:12:36 -0400

    chage some surround keymaps

--- kick/.config/kick/lua/plugins/surround.lua
+++ kick/.config/kick/lua/plugins/surround.lua
@@ -1,24 +1,27 @@
 return {
   {
-    "kylechui/nvim-surround",
-    keys = { "s", "d", "c" },
+    'kylechui/nvim-surround',
+    keys = { 's', 'd', 'c' },
     ---@type user_options The user options.
     ---@diagnostic disable-next-line: missing-fields
     opts = {
       move_cursor = false,
       keymaps = {
-        normal = "s",
-        visual = "s",
+        normal = 's',
+        normal_cur = 'ss',
+        normal_line = 'S',
+        normal_cur_line = 'SS',
+        visual = 's',
       },
       aliases = {
-        ["s"] = "]", -- Square brackets
-        ["p"] = ")", -- Paren
-        ["b"] = "}", -- Brackets
-        ["q"] = '"', -- Quotes
-        ["m"] = "$", -- Math (latex)
+        ['s'] = ']', -- Square brackets
+        ['p'] = ')', -- Paren
+        ['b'] = '}', -- Brackets
+        ['q'] = '"', -- Quotes
+        ['m'] = '$', -- Math (latex)
       },
       highlight = {
-        duration = 0,
+        duration = 10,
       },
     },
   },
