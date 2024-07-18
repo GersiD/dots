5fb035f936914d36dc1a1621c7cac3c1e519096c
Gersi.doko <Gersi.doko@unh.edu>
2024-07-18 15:11:22 -0400

    git diff keymap on diffview

--- kick/.config/kick/lua/config/keymaps.lua
+++ kick/.config/kick/lua/config/keymaps.lua
@@ -161,18 +161,6 @@ vim.keymap.set('n', 'K', function()
   end
 end, { desc = 'DAP Eval' })
 
--- Git Keymaps
-vim.keymap.set('n', '<leader>gd', function()
-  local view = require('diffview.lib').get_current_view()
-  if view then
-    -- Current tabpage is a Diffview; close it
-    vim.cmd(':DiffviewClose')
-  else
-    -- No open Diffview exists: open a new one
-    vim.cmd(':DiffviewOpen')
-  end
-end, { desc = 'Diffview' })
-
 -- Buffer keymaps
 -- Delete all buffers except current
 vim.keymap.set('n', '<leader>bD', function()
