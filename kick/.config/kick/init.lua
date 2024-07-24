b8e5eb04e1a35a5c9fbf8c1995e6ec5077a0e6f7
Gersi.doko <Gersi.doko@unh.edu>
2024-07-23 20:39:36 -0400

    big overhaul to the init file

--- kick/.config/kick/init.lua
+++ kick/.config/kick/init.lua
@@ -1,3 +1,6 @@
+-- Use lazy lua loader
+vim.loader.enable()
+
 -- Must include options config before anything loads
 --NOTE: set all options before lazy
 require('config.options')
@@ -20,7 +23,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   spec = {
     -- Detect tabstop and shiftwidth automatically
-    { 'tpope/vim-sleuth', lazy = false },
+    { 'tpope/vim-sleuth', event = 'VeryLazy' },
     { import = 'plugins' },
     { import = 'plugins.lsp' },
     { import = 'plugins.ui' },
@@ -34,48 +37,17 @@ require('lazy').setup({
 -- Set Colorscheme
 -- See `:help colorscheme`
 vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol', { fg = '#FFFFFF' })
-vim.cmd.colorscheme('catppuccin')
-require('config.keymaps')
-
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
-
--- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
-
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
-
--- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]resume' })
-vim.keymap.set('n', '<leader>sm', function()
-  require('telescope.builtin').man_pages({ sections = { 'ALL' } })
-end, { desc = '[S]earch [M]an pages' })
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
+vim.schedule(function()
+  vim.cmd.colorscheme('catppuccin')
+end)
+-- Load the keymaps
+vim.schedule(function()
+  require('config.keymaps')
+end)
 -- Load the autocommands
-require('config.autocmds')
+vim.schedule(function()
+  require('config.autocmds')
+end)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
