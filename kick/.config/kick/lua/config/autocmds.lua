132602cd8261c797fccaccd7d83160c03107971c
Gersi.doko <Gersi.doko@unh.edu>
2024-07-23 20:38:38 -0400

    move highligh on yank to the autocomands config file

--- kick/.config/kick/lua/config/autocmds.lua
+++ kick/.config/kick/lua/config/autocmds.lua
@@ -52,3 +52,13 @@ vim.api.nvim_create_autocmd('InsertCharPre', {
     vim.api.nvim_input("<Esc>m'" .. row + 1 .. 'gg' .. col + 1 .. "|if<Esc>`'la")
   end,
 })
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
