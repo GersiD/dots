ac489e60193c1457bedfd95b22801ad499ccb3bb
Gersi.doko <Gersi.doko@unh.edu>
2024-07-30 15:29:57 -0400

    remove default leader 1 keybind
    I never use the default one, I always use the buffer local one

--- kick/.config/kick/lua/config/keymaps.lua
+++ kick/.config/kick/lua/config/keymaps.lua
@@ -80,11 +80,6 @@ vim.keymap.set('n', '<leader>lS', '<cmd>LspStart<cr>', { desc = 'LSP Start' })
 vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, { desc = 'LSP Diag' })
 -- vim.keymap.set("n", "<leader>lf", require("lazyvim.plugins.lsp.format").format, { desc = "LSP Format" })
 vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, { desc = 'LSP Rename' })
-vim.keymap.set('n', '<leader>1', function()
-  vim.ui.input({ prompt = 'Run Command: ' }, function(cmd)
-    require('config.utils.terminals').run(cmd)
-  end)
-end, { desc = 'Run Command' })
 vim.keymap.set('n', 'gt', function()
   -- I want <CR> to open the selection in a vertical split
   require('telescope.builtin').lsp_type_definitions(require('telescope.themes').get_cursor({
