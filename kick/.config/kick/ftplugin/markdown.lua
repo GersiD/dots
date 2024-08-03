44440f401aa0811f61dab988685883662cd91950
Gersi.doko <Gersi.doko@unh.edu>
2024-08-02 20:15:38 -0400

    modernize obsidian.nvim config

--- kick/.config/kick/ftplugin/markdown.lua
+++ kick/.config/kick/ftplugin/markdown.lua
@@ -8,3 +8,19 @@ vim.keymap.set('n', 'gC', function()
   vim.cmd('ObsidianToggleCheckbox')
   vim.cmd('ObsidianToggleCheckbox')
 end, { desc = 'Complete checkbox item', noremap = true, silent = true })
+
+vim.keymap.set('n', 'gt', function()
+  vim.cmd('ObsidianTags')
+end, { desc = 'Goto Obsidian Tags', noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>ml', function()
+  vim.cmd('ObsidianQuickSwitch')
+end, { desc = 'Markdown Link', noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>mc', function()
+  vim.cmd('ObsidianCheck')
+end, { desc = 'Markdown Check', noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>mt', function()
+  vim.cmd('ObsidianTags')
+end, { desc = 'Markdown Tags', noremap = true, silent = true })
--- kick/.config/kick/lua/plugins/lsp/obsidian.lua
+++ kick/.config/kick/lua/plugins/lsp/obsidian.lua
@@ -14,6 +14,7 @@ return {
     -- Required.
     'nvim-lua/plenary.nvim',
   },
+  ---@type obsidian.config.ClientOpts | table<string, any>
   opts = {
     workspaces = {
       {
@@ -21,12 +22,57 @@ return {
         path = '~/vaults/gersi_notes/',
       },
     },
+    completion = {
+      nvim_cmp = true,
+      min_chars = 0,
+    },
     mappings = {
       ['gd'] = {
         action = function()
           return require('obsidian').util.gf_passthrough()
         end,
         opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      ['<cr>'] = {
+        action = function()
+          return require('obsidian').util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ''
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+      else
+        -- If title is nil, ask user for a tile.
+        suffix = vim.fn.input({ prompt = 'Commit message: ', default = nil })
+        -- If user cancels, generate a random 4 letter suffix.
+        if suffix == nil then
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+      end
+      return suffix
+    end,
+    picker = {
+      name = 'telescope.nvim',
+      note_mappings = {
+        new = '<C-n>',
+        insert_link = '<CR>',
+      },
+      tag_mappings = {
+        tag_note = '<C-n>',
+        insert_tag = '<C-i>',
       },
     },
   },
