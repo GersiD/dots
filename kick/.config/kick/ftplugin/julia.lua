1bf27b8cc1c1c3146b3b220406f786a87c736a45
Gersi.doko <Gersi.doko@unh.edu>
2024-08-03 20:50:11 -0400

    add julia test line

--- kick/.config/kick/ftplugin/julia.lua
+++ kick/.config/kick/ftplugin/julia.lua
@@ -1,3 +1,6 @@
 vim.keymap.set('n', '<leader>1', function()
   require('config.utils.terminals').run('time julia' .. ' ' .. vim.fn.expand('%'))
 end, { desc = 'Run Julia File', buffer = true })
+vim.keymap.set('n', '<leader>2', function()
+  require('config.utils.terminals').run('time julia runtests.jl')
+end, { desc = 'Run Julia File', buffer = true })
