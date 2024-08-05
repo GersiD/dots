49ffae250cddb7a61be6662dc199422c2944de84
Gersi.doko <Gersi.doko@unh.edu>
2024-08-04 20:41:15 -0400

    feat : support go files

---
+++ kick/.config/kick/ftplugin/go.lua
@@ -0,0 +1,6 @@
+vim.keymap.set('n', '<leader>1', function()
+  require('config.utils.terminals').run('time go run .')
+end, { desc = 'Run Go File', buffer = true })
+vim.keymap.set('n', '<leader>2', function()
+  require('config.utils.terminals').run('time go test')
+end, { desc = 'Run Go Tests', buffer = true })
