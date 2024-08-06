57686b90ae2b42e11d81a10b724900e0824c5d13
Gersi.doko <Gersi.doko@unh.edu>
2024-08-06 14:19:49 -0400

    no need for leader s keymap for latex files

--- kick/.config/kick/ftplugin/tex.lua
+++ kick/.config/kick/ftplugin/tex.lua
@@ -2,7 +2,3 @@ vim.keymap.set('n', '<leader>1', function()
   vim.cmd('VimtexView')
 end, { desc = 'Compile Latex File', buffer = true })
 vim.opt.conceallevel = 0
-
-vim.keymap.set('n', '<leader>ls', function()
-  vim.cmd('LspStart ltex')
-end, { desc = 'Start Ltex (spell check)', buffer = true })
