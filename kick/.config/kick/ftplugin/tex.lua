893a3396769f246f060b2467d83920b7afb0e12e
Gersi.doko <Gersi.doko@unh.edu>
2024-11-01 20:07:29 -0400

    add keybinds for latex dev

--- kick/.config/kick/ftplugin/tex.lua
+++ kick/.config/kick/ftplugin/tex.lua
@@ -1,4 +1,15 @@
 vim.keymap.set('n', '<leader>1', function()
   vim.cmd('VimtexView')
 end, { desc = 'Compile Latex File', buffer = true })
+vim.keymap.set('n', '<leader>lc', function()
+  vim.cmd('VimtexClean')
+  -- Delete the build folder if it exists
+  vim.fn.delete(vim.fn.expand('%:p:h') .. '/build', 'rf')
+end, { desc = 'Compile Latex File', buffer = true })
+vim.keymap.set('n', '<leader>lq', function()
+  vim.diagnostic.setqflist({
+    namespace = vim.api.nvim_get_namespaces()['vim.lsp.ltex.3'],
+    severity = vim.diagnostic.severity.HINT,
+  })
+end, { desc = 'Show all diagnostics in quickfix' })
 vim.opt.conceallevel = 0
