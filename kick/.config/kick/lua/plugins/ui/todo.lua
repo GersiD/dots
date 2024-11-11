85d51cc48cc09d5571d3b27f5f27816789311f2f
Gersi.doko <Gersi.doko@unh.edu>
2024-11-11 14:06:27 -0500

    addback todo comments by folke <3

--- kick/.config/kick/lua/plugins/ui/todo.lua
+++ kick/.config/kick/lua/plugins/ui/todo.lua
@@ -1,16 +1,66 @@
 return {
-  'folke/todo-comments.nvim',
-  cmd = { 'TodoTrouble', 'TodoTelescope' },
-  event = 'VeryLazy',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = {},
-      -- stylua: ignore
-      keys = {
-              { "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
-              { "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-              { "<leader>xx", "<cmd>TodoQuickFix<cr>",                             desc = "Todo (Trouble)" },
-              { "<leader>xX", "<cmd>TodoQuickFix keywords=TODO,FIX,FIXME<cr>",     desc = "Todo/Fix/Fixme (Trouble)" },
-              { "<leader>st", "<cmd>TodoTelescope<cr>",                            desc = "Todo" },
-              { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",    desc = "Todo/Fix/Fixme" },
+  {
+    'folke/todo-comments.nvim',
+    cmd = { 'TodoTrouble', 'TodoTelescope' },
+    event = 'VeryLazy',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {},
+    keys = {
+      {
+        ']t',
+        function()
+          require('todo-comments').jump_next()
+        end,
+        desc = 'Next todo comment',
       },
+      {
+        '[t',
+        function()
+          require('todo-comments').jump_prev()
+        end,
+        desc = 'Previous todo comment',
+      },
+      { '<leader>xt', '<cmd>TodoQuickFix<cr>', desc = 'Todo (Trouble)' },
+      { '<leader>xT', '<cmd>TodoQuickFix keywords=TODO,FIX,FIXME<cr>', desc = 'Todo/Fix/Fixme (Trouble)' },
+      { '<leader>st', '<cmd>TodoTelescope<cr>', desc = 'Todo' },
+      { '<leader>sT', '<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>', desc = 'Todo/Fix/Fixme' },
+    },
+  },
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+  },
 }
