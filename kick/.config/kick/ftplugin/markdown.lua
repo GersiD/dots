33a90e8e6d95acff4ed732e115c0a56d5ce83988
Gersi.doko <Gersi.doko@unh.edu>
2024-11-20 14:07:05 -0500

    <leader>mf (markdown find) for obsidian notes

--- kick/.config/kick/ftplugin/markdown.lua
+++ kick/.config/kick/ftplugin/markdown.lua
@@ -25,6 +25,10 @@ vim.keymap.set('n', '<leader>mt', function()
   vim.cmd('ObsidianTags')
 end, { desc = 'Markdown Tags', noremap = true, silent = true })
 
+vim.keymap.set('n', '<leader>mf', function()
+  vim.cmd('ObsidianSearch')
+end, { desc = 'Find Obsidian', noremap = true, silent = true })
+
 vim.keymap.set('n', '<leader>mo', function()
   vim.cmd('ObsidianOpen')
 end, { desc = 'Open in Obsidian', noremap = true, silent = true })
