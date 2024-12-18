1af094538a6bfc5723408da02ecb5e96e6ffe5e0
Gersi.doko <Gersi.doko@unh.edu>
2024-12-17 20:20:29 -0500

    fix luasnip

--- kick/.config/kick/lua/plugins/luasnip.lua
+++ kick/.config/kick/lua/plugins/luasnip.lua
@@ -1,27 +1,25 @@
 return {
-  "L3MON4D3/LuaSnip",
+  'L3MON4D3/LuaSnip',
   opts = function()
-    local types = require("luasnip.util.types")
+    local types = require('luasnip.util.types')
     return {
-    history = true,
-    delete_check_events = "TextChanged",
-    ext_opts = {
-      [types.insertNode] = {
-        unvisited = {
-          virt_text = { { "|", "Conceal" } },
-          virt_text_pos = "inline",
+      ext_opts = {
+        [types.insertNode] = {
+          unvisited = {
+            virt_text = { { '|', 'Conceal' } },
+            virt_text_pos = 'inline',
+          },
+        },
+        -- This is needed because LuaSnip differentiates between $0 and other
+        -- placeholders (exitNode and insertNode). So this will highlight the last
+        -- jump node.
+        [types.exitNode] = {
+          unvisited = {
+            virt_text = { { '|', 'Conceal' } },
+            virt_text_pos = 'inline',
+          },
         },
       },
-      -- This is needed because LuaSnip differentiates between $0 and other
-      -- placeholders (exitNode and insertNode). So this will highlight the last
-      -- jump node.
-      [types.exitNode] = {
-        unvisited = {
-          virt_text = { { "|", "Conceal" } },
-          virt_text_pos = "inline",
-        },
-      },
-    },
-  }
-end
+    }
+  end,
 }
