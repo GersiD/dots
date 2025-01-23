d396f869dba3998adec463f001cb2629ee668b5a
Gersi.doko <Gersi.doko@unh.edu>
2025-01-23 10:48:30 -0500

    small change

--- kick/.config/kick/lua/plugins/ui/themes.lua
+++ kick/.config/kick/lua/plugins/ui/themes.lua
@@ -50,7 +50,7 @@ return {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       style = 'moon', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-      -- terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
       -- no_italic = true, -- Disable italic comments, keywords, etc.
       transparent = false, -- Enable transparent background
       styles = {
