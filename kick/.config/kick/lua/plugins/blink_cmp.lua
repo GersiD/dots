86762cba2a31aaa2a38d816ec8779c684451ee82
Gersi.doko <Gersi.doko@unh.edu>
2025-02-06 09:30:05 -0500

    new fonts!

--- kick/.config/kick/lua/plugins/blink_cmp.lua
+++ kick/.config/kick/lua/plugins/blink_cmp.lua
@@ -35,7 +35,7 @@ return {
       use_nvim_cmp_as_default = true,
       -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono',
+      nerd_font_variant = 'normal',
       kind_icons = require('config.icons').kinds,
     },
     completion = {
