d51ec6078ff67d406858a757e1b833b6236d24cf
Gersi.doko <Gersi.doko@unh.edu>
2025-02-10 18:29:48 -0500

    blini

--- kick/.config/kick/lua/plugins/blink_cmp.lua
+++ kick/.config/kick/lua/plugins/blink_cmp.lua
@@ -74,7 +74,7 @@ return {
       },
       documentation = {
         auto_show = true,
-        auto_show_delay_ms = 200,
+        auto_show_delay_ms = 300,
         window = {
           border = 'rounded',
         },
