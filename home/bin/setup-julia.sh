d6c9cbbe118e63c9d706469a6014565a30f1c3e1
Gersi.doko <Gersi.doko@unh.edu>
2025-02-10 19:49:36 -0500

    up

--- home/bin/setup-julia.sh
+++ home/bin/setup-julia.sh
@@ -2,7 +2,9 @@
 # setup the Language Server Protocol for Julia
 # and init the current working directory
 # Usage: setup-julia.sh
+echo "Installing and updating Julia Language Server"
 julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer")'
 julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.update()'
+echo "Initializing the current working directory"
 julia --project=. -e 'using Pkg; Pkg.instantiate()'
 julia --project=. -e 'using Pkg; Pkg.precompile()'
