0d0d5139ed1d3d4ef3aad595e1f2cca5866338e1
Gersi.doko <Gersi.doko@unh.edu>
2025-03-14 20:41:03 -0400

    pcall everything in init.lua
    helps if there is an error in one part of your config not the whole
    thing breaks

--- kick/.config/nvim/init.lua
+++ kick/.config/nvim/init.lua
@@ -3,7 +3,10 @@ vim.loader.enable()
 
 -- Must include options config before anything loads
 --NOTE: set all options before lazy
-require('config.options')
+local ok, err = pcall(require, 'config.options')
+if not ok then
+  vim.notify('init.lua: Error loading options ' .. err, vim.log.levels.ERROR, { title = 'Config Error' })
+end
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
@@ -34,14 +37,27 @@ require('lazy').setup({
 
 -- Set Colorscheme
 -- See `:help colorscheme`
+local colorscheme = 'gruvbox-material'
 vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol', { fg = '#FFFFFF' })
-vim.cmd.colorscheme('gruvbox-material')
+local ok, err = pcall(vim.cmd.colorscheme, colorscheme)
+if not ok then
+  vim.notify('init.lua: Error loading colorscheme ' .. err, vim.log.levels.ERROR, { title = 'Config Error' })
+end
 -- Load the highlight overrides
-require('config.highlights')
+local ok, err = pcall(require, 'config.highlights')
+if not ok then
+  vim.notify('init.lua: Error loading highlights ' .. err, vim.log.levels.ERROR, { title = 'Config Error' })
+end
 -- Load the keymaps
-require('config.keymaps')
+local ok, err = pcall(require, 'config.keymaps')
+if not ok then
+  vim.notify('init.lua: Error loading keymaps ' .. err, vim.log.levels.ERROR, { title = 'Config Error' })
+end
 -- Load the autocommands
-require('config.autocmds')
+local ok, err = pcall(require, 'config.autocmds')
+if not ok then
+  vim.notify('init.lua: Error loading autocmds ' .. err, vim.log.levels.ERROR, { title = 'Config Error' })
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
