7b1036829b6fc976ebb4555cad05c282ec7c1e85
Gersi.doko <Gersi.doko@unh.edu>
2025-03-18 10:59:27 -0400

    grug now works well for me

--- kick/.config/nvim/lua/plugins/find_replace.lua
+++ kick/.config/nvim/lua/plugins/find_replace.lua
@@ -1,6 +1,34 @@
 return {
   'MagicDuck/grug-far.nvim',
-  opts = { headerMaxWidth = 80 },
+  ---@type GrugFarOptions
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    headerMaxWidth = 80,
+    visualSelectionUsage = 'operate-within-range',
+    keymaps = {
+      previewLocation = { n = '<localleader>i' },
+      applyNext = { n = '<C-j>' },
+      applyPrev = { n = '<C-k>' },
+      close = { n = 'q' },
+      replace = { n = '<leader>r' },
+      qflist = { n = '<C-q>' },
+      syncLocations = { n = '<C-s>' },
+      syncLine = { n = '<localleader>l' },
+      openLocation = { n = '<localleader>o' },
+      openNextLocation = { n = '<down>' },
+      openPrevLocation = { n = '<up>' },
+      gotoLocation = { n = '<enter>' },
+      abort = { n = '<C-c>' },
+      pickHistoryEntry = { n = '<enter>' },
+      help = { n = 'g?' },
+      toggleShowCommand = { n = '<localleader>p' },
+      swapEngine = { n = '<localleader>e' },
+      swapReplacementInterpreter = { n = '<localleader>x' },
+      historyOpen = { n = '<localleader>t' },
+      historyAdd = { n = '<localleader>a' },
+      refresh = { n = '<localleader>r' },
+    },
+  },
   cmd = 'GrugFar',
   keys = {
     {
