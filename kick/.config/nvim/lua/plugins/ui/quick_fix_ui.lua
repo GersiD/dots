89710e73c55ae12c7012facf8d2aabcab4e70dcc
Gersi.doko <Gersi.doko@unh.edu>
2025-03-18 16:35:45 -0400

    trying to see whats up with quicker its not doingwhat I wanted

--- kick/.config/nvim/lua/plugins/ui/quick_fix_ui.lua
+++ kick/.config/nvim/lua/plugins/ui/quick_fix_ui.lua
@@ -1,30 +1,23 @@
 return {
-  {
-    'stevearc/quicker.nvim',
-    event = 'FileType qf',
-    ---@module "quicker"
-    ---@type quicker.SetupOptions
-    opts = {
-      edit = {
-        enabled = true,
-      },
-      highlight = {
-        load_buffers = false,
-        treesitter = true,
-        lsp = true,
-      },
-      follow = {
-        enabled = true,
-      },
-      -- on_qf = function(bufnr)
-      --   -- require('quicker').toggle_expand({}) -- TODO: Doesnt work on latex files sometimes
-      -- end,
-      keys = {
-        { '>', "<cmd>lua require('quicker').toggle_expand()<CR>", desc = 'Expand quickfix content' },
-      },
-      max_filename_width = function()
-        return 15
-      end,
+  'stevearc/quicker.nvim',
+  event = 'FileType qf',
+  ---@module "quicker"
+  ---@type quicker.SetupOptions
+  opts = {
+    edit = { enabled = true },
+    highlight = {
+      load_buffers = false,
+      treesitter = true,
+      lsp = true,
     },
+    follow = {
+      enabled = true,
+    },
+    keys = {
+      { '>', "<cmd>lua require('quicker').toggle_expand()<CR>", desc = 'Expand quickfix content' },
+    },
+    max_filename_width = function()
+      return 15
+    end,
   },
 }
