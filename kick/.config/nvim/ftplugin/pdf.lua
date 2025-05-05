1381a70465920bcefabdecbc769bf32d315145cc
Gersi.doko <Gersi.doko@unh.edu>
2025-05-05 11:16:06 -0400

    for pdf files, q should just delete the current buff

--- kick/.config/nvim/ftplugin/pdf.lua
+++ kick/.config/nvim/ftplugin/pdf.lua
@@ -1,6 +1,10 @@
 vim.keymap.set('n', '<leader>1', function()
   vim.cmd("!zathura '%' &")
 end, { desc = 'Open PDF', buffer = true })
+vim.keymap.set('n', 'q', function()
+  -- delete the current buffer
+  vim.cmd('bdelete')
+end, { desc = 'Open PDF', buffer = true })
 -- NOTE: Even if the opts to a vim.keymap call are incorrect or do not exist
 -- The entire call will fail silently and the keymap will not be set
 -- Ask me how I know...
