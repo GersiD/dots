27fa3d4f8212b122490b116707c065637fc1c8fd
Gersi.doko <Gersi.doko@unh.edu>
2025-06-23 09:52:14 -0400

    switch to tw for our csv viewer

--- kick/.config/nvim/ftplugin/csv.lua
+++ kick/.config/nvim/ftplugin/csv.lua
@@ -1,6 +1,7 @@
 -- Open all CSV files with CSV Lens
 vim.keymap.set('n', '<leader>1', function()
-  require('config.utils.terminals').run('csvlens --color-columns ' .. vim.fn.expand('%')) -- Requires csvlens 12.0.2 or later
+  -- require('config.utils.terminals').run('csvlens --color-columns ' .. vim.fn.expand('%')) -- Requires csvlens 12.0.2 or later
+  require('config.utils.terminals').run('tw ' .. vim.fn.expand('%')) -- Requires csvlens 12.0.2 or later
 end, { desc = 'Open CSV File', buffer = true })
 vim.keymap.set('n', 'q', function()
   -- delete the current buffer
